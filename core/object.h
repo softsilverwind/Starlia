a9752b2efb1b4ca303ee99240a73c788dcff64e6
Nick Vathis <nvathis@gmail.com>
2012-05-30 17:05:33 +0300

    removed some lines

--- core/object.h
+++ core/object.h
@@ -51,24 +51,18 @@ class Star2dObject : public StarObject
 		Star2dObject(Coord2d position, Coord2d radius, double angle = 0, Star2dModel *model = NULL);
 		~Star2dObject();
 
-		void setPosition(const Coord2d&&);
 		void setPosition(const Coord2d&);
 		const Coord2d& getPosition() const;
-		void setAngle(const double&&);
 		void setAngle(const double&);
 		double getAngle() const;
-		void setRadius(const Coord2d&&);
 		void setRadius(const Coord2d&);
 		const Coord2d& getRadius() const;
 };
 
-inline void Star2dObject::setPosition(const Coord2d&& pos) { position = pos; }
 inline void Star2dObject::setPosition(const Coord2d& pos) { position = pos; }
 inline const Coord2d& Star2dObject::getPosition() const { return position; }
-inline void Star2dObject::setAngle(const double&& ang) { angle = ang; }
 inline void Star2dObject::setAngle(const double& ang) { angle = ang; }
 inline double Star2dObject::getAngle() const { return angle; }
-inline void Star2dObject::setRadius(const Coord2d&& rad) { radius = rad; }
 inline void Star2dObject::setRadius(const Coord2d& rad) { radius = rad; }
 inline const Coord2d& Star2dObject::getRadius() const { return radius; }
 
@@ -84,18 +78,14 @@ class Star2dDynObject : public Star2dObject
 	public:
 		Star2dDynObject(Coord2d position, Coord2d radius, double angle = 0, Star2dModel *model = NULL);
 
-		void setVelocity(const Coord2d&&);
 		void setVelocity(const Coord2d&);
 		const Coord2d& getVelocity() const;
-		void setAngVelocity(const double&&);
 		void setAngVelocity(const double&);
 		double getAngVelocity() const;
 };
 
-inline void Star2dDynObject::setVelocity(const Coord2d&& vel) { velocity = vel; }
 inline void Star2dDynObject::setVelocity(const Coord2d& vel) { velocity = vel; }
 inline const Coord2d& Star2dDynObject::getVelocity() const { return velocity; }
-inline void Star2dDynObject::setAngVelocity(const double&& angvel) { angvelocity = angvel; }
 inline void Star2dDynObject::setAngVelocity(const double& angvel) { angvelocity = angvel; }
 inline double Star2dDynObject::getAngVelocity() const { return angvelocity; }
 
@@ -141,24 +131,18 @@ class Star3dObject : public StarObject
 		Coord3d getNormalY() const;
 		Coord3d getNormalZ() const;
 
-		void setPosition(const Coord3d&&);
 		void setPosition(const Coord3d&);
 		const Coord3d& getPosition() const;
-		void setAngle(const Coord3d&&);
 		void setAngle(const Coord3d&);
 		const Coord3d& getAngle() const;
-		void setRadius(const Coord3d&&);
 		void setRadius(const Coord3d&);
 		const Coord3d& getRadius() const;
 };
 
-inline void Star3dObject::setPosition(const Coord3d&& pos) { position = pos; }
 inline void Star3dObject::setPosition(const Coord3d& pos) { position = pos; }
 inline const Coord3d& Star3dObject::getPosition() const { return position; }
-inline void Star3dObject::setAngle(const Coord3d&& ang) { angle = ang; }
 inline void Star3dObject::setAngle(const Coord3d& ang) { angle = ang; }
 inline const Coord3d& Star3dObject::getAngle() const { return angle; }
-inline void Star3dObject::setRadius(const Coord3d&& rad) { radius = rad; }
 inline void Star3dObject::setRadius(const Coord3d& rad) { radius = rad; }
 inline const Coord3d& Star3dObject::getRadius() const { return radius; }
 
@@ -176,24 +160,18 @@ class Star3dDynObject : public Star3dObject
 	public:
 		Star3dDynObject(Coord3d position, Coord3d radius, Coord3d angle, Star3dModel *model = NULL);
 
-		void setVelocity(const Coord3d&&);
 		void setVelocity(const Coord3d&);
 		const Coord3d& getVelocity() const;
-		void setAngVelocity(const Coord3d&&);
 		void setAngVelocity(const Coord3d&);
 		const Coord3d& getAngVelocity() const;
-		void setThrust(const Coord3d&&);
 		void setThrust(const Coord3d&);
 		const Coord3d& getThrust() const;
 };
 
-inline void Star3dDynObject::setVelocity(const Coord3d&& vel) { velocity = vel; }
 inline void Star3dDynObject::setVelocity(const Coord3d& vel) { velocity = vel; }
 inline const Coord3d& Star3dDynObject::getVelocity() const { return velocity; }
-inline void Star3dDynObject::setAngVelocity(const Coord3d&& angvel) { angvelocity = angvel; }
 inline void Star3dDynObject::setAngVelocity(const Coord3d& angvel) { angvelocity = angvel; }
 inline const Coord3d& Star3dDynObject::getAngVelocity() const { return angvelocity; }
-inline void Star3dDynObject::setThrust(const Coord3d&& thr) { thrust = thr; }
 inline void Star3dDynObject::setThrust(const Coord3d& thr) { thrust = thr; }
 inline const Coord3d& Star3dDynObject::getThrust() const { return thrust; }
 
