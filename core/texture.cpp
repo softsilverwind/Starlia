de737c657e57edbc792449a0df06a520c6ae7912
Nick Vathis <nvathis@gmail.com>
2012-10-03 18:52:48 +0300

    load tex with sdl_image

--- core/texture.cpp
+++ core/texture.cpp
@@ -1,7 +1,5 @@
-#include <cstdio>
-#include <cstdlib>
+#include <SDL/SDL_image.h>
 #include <GL/glu.h>
-#include <fstream>
 #include <cassert>
 #include "texture.h"
 #include "structs.h"
@@ -11,50 +9,44 @@ using namespace std;
 namespace Starlia
 {
 
-unsigned int genBMPTex(string filename)
+unsigned int genTex(string filename)
 {
-	ifstream file(filename.c_str(), ios::binary);
-	unsigned int sizex, sizey;
-	char *img;
-	unsigned short bpp;
-	unsigned int siz;
-
+	SDL_Surface *img = IMG_Load(filename.c_str());
 	unsigned int texture;
+	int mode;
 
-	file.seekg(18, ios::cur);
+	if (!img)
+		return 0;
 
-	file.read((char *) &sizex, 4);
-	file.read((char *) &sizey, 4);
+	if (img->format->BytesPerPixel == 3)
+	{
+		mode = GL_RGB;
+	}
+	else if (img->format->BytesPerPixel == 4)
+	{
+		mode = GL_RGBA;
+	}
+	else
+	{
+		SDL_FreeSurface(img);
+		return 0;
+	}
 
-	file.seekg(2, ios::cur);
-	file.read((char *) &bpp, 2);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
-	file.seekg(24, ios::cur);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	siz = sizex * sizey * 3;
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	img = new char[siz];
+	gluBuild2DMipmaps(GL_TEXTURE_2D, mode, img->w, img->h, mode, GL_UNSIGNED_BYTE, img->pixels);
 
-	file.read(img, siz);
+	glBindTexture(GL_TEXTURE_2D, 0);
 
-	for (unsigned int i = 0; i < siz; i += 3)
-		swap(img[i], img[i+2]);
-
-	glGenTextures( 1, &texture );
-	glBindTexture( GL_TEXTURE_2D, texture );
-
-	glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
-	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST );
-	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-
-	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	gluBuild2DMipmaps( GL_TEXTURE_2D, 3, sizex, sizey, GL_RGB, GL_UNSIGNED_BYTE, img );
-
-	glBindTexture( GL_TEXTURE_2D, 0 );
-
-	delete [] img;
+	SDL_FreeSurface(img);
 
 	return texture;
 }
